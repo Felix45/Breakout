97b7939a0d707f16cce58c55c2c076f6a96dc6f1
Felix Ouma <fatonoh@gmail.com>
2022-07-01 01:04:36 +0300

    Add game level to score board

--- assets/js/modules/scoreboard.js
+++ assets/js/modules/scoreboard.js
@@ -8,7 +8,8 @@ class ScoreBoard {
   draw(ctx) {
     ctx.font = 'bold 20px Courier';
     ctx.fillStyle = '#000';
-    ctx.fillText(`Score: ${this.gamescene.score} Lives: ${this.gamescene.lives}`, this.gamewidth - 200, 30, 400);
+    ctx.fillText(`Score: ${this.gamescene.score} Lives: ${this.gamescene.lives} Level: ${this.gamescene.level + 1}`,
+      this.gamewidth - 200, 30, 400);
   }
 
   update() {
